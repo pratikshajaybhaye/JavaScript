6b6c17ec72700e24f7a010a11ae02153d5be7fff
pratikshajaybhaye <pratikshajaybhaye1210@gmail.com>
2023-12-28 21:10:49 +0530

    Assignment 1
    Assignment 1

---
+++ 01_variableAssign.js
@@ -0,0 +1,35 @@
+
+
+console.log("==========Declare Variable============");
+ var myFullName;
+ console.log(myFullName);
+
+ console.log("==========Initilization Variable============");
+var myFullName = " Pratiksha Jaybhaye"
+console.log("my FullName is",myFullName);
+
+
+ console.log("==========Update Variable============");
+ var myFullName = " Pratiksha  Mhatardev Jaybhaye"
+console.log("my FullName is",myFullName);
+
+console.log("==========Step 2============");
+var myLove = "I Love Only JS"
+console.log("Result ", myLove);
+
+console.log("==========Step 3============");
+var  firstName = "Pratiksha"
+var lastName = "Jaybhaye"
+var age = "24"
+var address = " Nerul, Navi Mumbai"
+var pinCode ="400706"
+var gender = " Female "
+var isMarried = "No"
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("PinCode:", pinCode);
+console.log("Gender", gender);
+console.log("Married:", isMarried);
+
---
+++ 01_variableSwap.js
@@ -0,0 +1,17 @@
+// Before Swap
+console.log("Before Swap");
+n1 = "10";
+n2 = "20";
+console.log("n1-->", n1);
+console.log("n2-->", n2);
+
+// After Swap
+
+console.log("After Swap");
+var temp = n1;
+n1 = n2
+n2 = temp;
+console.log("n1-->", n1);
+console.log("n2-->", n2);
+
+
