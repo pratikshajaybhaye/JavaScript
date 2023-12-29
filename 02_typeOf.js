7ba3cadf931ce16a44f7b45d35c5f61f000ddc80
pratikshajaybhaye <pratikshajaybhaye1210@gmail.com>
2023-12-30 00:04:11 +0530

    JS Assignment 2
    JS Assignment 2

---
+++ 02_typeOf.js
@@ -0,0 +1,49 @@
+console.log("===========Step 1================");
+
+console.log("===========Before Intialization================");
+var bankName;
+console.log("Bank Name:",bankName);
+console.log("typeof bankName:",bankName);
+console.log("===========After Intialization================");
+var bankName = "SBI Bank";
+console.log("Bank Name:",bankName);
+console.log("typeof bankName:",typeof bankName);
+
+console.log("===========Step 2================");
+
+var nameMarks = '90 %';
+console.log("Name Marks:",nameMarks);
+console.log("typeof nameMarks:",typeof nameMarks);
+
+console.log("===========Step 3================");
+console.log("===========Before Intialization================");
+var isWorking;
+console.log("Is Working:",isWorking);
+console.log("typeof isWorking:",isWorking);
+
+console.log("===========Before Intialization================");
+var isWorking = true;
+console.log("Is Working:",isWorking);
+console.log("typeof isWorking:",typeof isWorking);
+
+console.log("===========Step 4================");
+var totalCount = "Hundred and Seven";
+console.log("Is totalCount:",totalCount);
+console.log("typeof totalCount:",typeof totalCount);
+
+console.log("===========Update Variable================");
+var totalCount = 107;
+console.log("Is totalCount:",totalCount);
+console.log("typeof totalCount:",typeof totalCount);
+
+
+
+
+
+
+
+
+
+
+
+
