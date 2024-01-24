34ed4a44b6ef9c60e3fadcf4d14954e291543ee0
pratikshajaybhaye <pratikshajaybhaye1210@gmail.com>
2024-01-25 00:06:32 +0530

    DOM Assignment
    DOM Assignment

---
+++ DOM/14_main.js
@@ -0,0 +1,7 @@
+
+const heading = document.querySelector('#profile');
+heading.style.color = 'Purple'
+
+const technology = document.querySelector('#stack')
+technology.style.color = 'Red'
+technology.style.fontSize = 'Larger'
